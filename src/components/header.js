c87c1f16863da4c330f2f8cd7e037ba74b4272b2
Fernando Malaman <contato.fernandom@gmail.com>
2018-09-13 22:47:18 -0300

    inclusão de assets e cabeçalho

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="ribbon">
+        <img
+          className="logo"
+          src="1200px-MarvelLogo.svg.png"
+          alt="marvel logo"
+        />
+      </header>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./components/app";
 
-ReactDOM.render(<div>Olá Munddo</div>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
