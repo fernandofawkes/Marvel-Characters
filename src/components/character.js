30c0a8af0618a637d912a97cae94e6f20f090f3b
Fernando Malaman <contato.fernandom@gmail.com>
2018-09-15 08:44:49 -0300

    espaçador do nome e descrição

--- src/components/character.js
+++ src/components/character.js
@@ -9,8 +9,9 @@ export default class Character extends React.Component {
     /**[TODO] separar */
     const { real, known } = extractNameParts(name);
     return (
-      <div className="character-block">
+      <div className="character">
         <img
+          className="character-image"
           src={`${path}/standard_medium.${extension}`}
           alt={`imagem do personagem ${name}`}
         />
@@ -18,7 +19,12 @@ export default class Character extends React.Component {
           <h2 className="character-name">{known}</h2>
           {real ? <h3 className="character-real-name">{real}</h3> : null}
         </hgroup>
-        <p className="character-desc">{description}</p>
+        {description ? (
+          <div className="character-details">
+            <div className="spacer" />
+            <p className="character-desc">{description}</p>
+          </div>
+        ) : null}
       </div>
     );
   }
