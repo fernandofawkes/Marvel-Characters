97d14a88ae98458e90251e9f355b1a06021e41d8
Fernando Malaman <contato.fernandom@gmail.com>
2018-09-14 23:24:51 -0300

    carregamento de thumnail e descrição dos personagens

--- src/components/character.js
+++ src/components/character.js
@@ -2,6 +2,17 @@ import React from "react";
 
 export default class Character extends React.Component {
   render() {
-    return <div className="character-block">{this.props.char.name}</div>;
+    const { name, description, thumbnail } = this.props.char;
+    const { path, extension } = thumbnail;
+    return (
+      <div className="character-block">
+        <img
+          src={`${path}/standard_medium.${extension}`}
+          alt={`imagem do personagem ${name}`}
+        />
+        <h2 className="character-name">{name}</h2>
+        <p className="character-desc">{description}</p>
+      </div>
+    );
   }
 }
