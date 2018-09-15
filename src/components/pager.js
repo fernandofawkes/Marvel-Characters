f906ecdf549dcdb8fa90a44d3c117fe8cc602fd0
Fernando Malaman <contato.fernandom@gmail.com>
2018-09-15 09:54:14 -0300

    ajuste da exibição de svgs

--- src/components/pager.js
+++ src/components/pager.js
@@ -1,19 +1,25 @@
 import React from "react";
+import LeftArrow from "../assets/arrow-left.svg";
+import RightArrow from "../assets/arrow-left.svg";
 
 export default class Pager extends React.Component {
   render() {
     return (
       <div className="pager">
-        <button type="button">{"<"}</button>
+        <button className="pager-button" type="button">
+          <LeftArrow />
+        </button>
         <input
           type="number"
           min="1"
           max={this.props.max}
-          value={this.props.page}
+          defaultValue={this.props.page}
           onChange={console.log}
         />
         <span className="pager-total">de {this.props.max}</span>
-        <button type="button">></button>
+        <button className="pager-button" type="button">
+          <RightArrow />
+        </button>
       </div>
     );
   }
